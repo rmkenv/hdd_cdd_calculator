116a5baebb45aabf246aefda91a59aec27b3120c
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2025-08-05 19:04:14 -0400

    Update __init__.py

--- hdd_cdd/__init__.py
+++ hdd_cdd/__init__.py
@@ -8,7 +8,7 @@ from .calculator import (
 )
 from .exceptions import NWSAPIError, InvalidCoordinatesError
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "calculate_degree_days",
