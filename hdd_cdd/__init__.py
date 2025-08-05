22afed107901f71452825086c2843dd45d444c6d
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2025-08-05 18:22:21 -0400

    Create __init__.py

---
+++ hdd_cdd/__init__.py
@@ -0,0 +1,19 @@
+"""A Python library for calculating Heating Degree Days (HDD) and Cooling Degree Days (CDD) using NWS API data."""
+
+from .calculator import (
+    calculate_degree_days,
+    get_degree_days_for_location,
+    get_degree_days_for_period,
+    DegreeDaysResult
+)
+from .exceptions import NWSAPIError, InvalidCoordinatesError
+
+__version__ = "0.1.0"
+__all__ = [
+    'calculate_degree_days',
+    'get_degree_days_for_location',
+    'get_degree_days_for_period',
+    'DegreeDaysResult',
+    'NWSAPIError',
+    'InvalidCoordinatesError'
+]
