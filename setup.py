74aed5a80fefbc76ee3aca70e5c7442e9f10d4b6
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2025-08-11 18:53:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,29 +5,42 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hdd-cdd-calculator",
-    version="0.1.2",
+    version="0.1.3",  # Updated to match __init__.py
     author="Ryan Kmetz",
     author_email="consultrmk@gmail.com",
-    description="A Python library for calculating Heating Degree Days (HDD) and Cooling Degree Days (CDD) using NWS API data",
+    description="A Python library for calculating Heating Degree Days (HDD) "
+                "and Cooling Degree Days (CDD) using NWS API data, "
+                "with support for other weather data sources like Meteostat.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rmkenv/hdd_cdd_calculator",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests*", "docs*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=[
-        "requests>=2.25.1",
+        "requests>=2.28",
+        "pandas>=1.3",
+        "meteostat>=1.6.5",
+        "python-dateutil>=2.8",
     ],
     extras_require={
         "dev": [
-            "pytest>=6.0",
-            "black>=21.0",
-            "flake8>=3.9",
-            "mypy>=0.900",
+            "pytest>=7.0",
+            "black>=23.0",
+            "flake8>=6.0",
+            "mypy>=1.0",
         ],
     },
+    include_package_data=True,
+    license="MIT",
 )
